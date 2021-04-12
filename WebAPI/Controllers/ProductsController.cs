536cd9d64cc8c4a5b8a8b30e69af2e66ed8af353
EsraOzdogan <esraozdogaan@gmail.com>
2021-04-13 01:46:09 +0300

    api

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]    //attribute
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Car> Get()
+        {
+            return "Merhaba";
+        }
+    }
+}
