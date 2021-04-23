fb2b2194c38811af6341a77251229a8e88093878
EsraOzdogan <esraozdogaan@gmail.com>
2021-04-23 23:17:43 +0300

    Refactoring

--- WebAPI/Controllers/CarImagesController.cs
+++ WebAPI/Controllers/CarImagesController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -46,6 +47,8 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
+            Thread.Sleep(1000);
+
             var result = _carImageService.GetAll();
             if (result.Success) return Ok(result);
 
